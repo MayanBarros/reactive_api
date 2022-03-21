5ddef70db0eca531c9ec35a7dc4690049c549980
Mayan Barros da Silva <mayanbarrosdasilva@gmail.com>
2022-03-21 09:28:46 -0300

    Project backend rest

--- src/main/java/com/webflux/reactiveapiwebflux/service/ConsultaService.java
+++ src/main/java/com/webflux/reactiveapiwebflux/service/ConsultaService.java
@@ -4,7 +4,6 @@ import com.webflux.reactiveapiwebflux.entity.ConsultaCpfCnpj;
 import com.webflux.reactiveapiwebflux.exception.CpfCnpjNotValidException;
 import com.webflux.reactiveapiwebflux.repository.ConsultaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -42,7 +41,6 @@ public class ConsultaService {
 
     public Mono<ConsultaCpfCnpj> saveNewConsulta(ConsultaCpfCnpj newConsulta) {
         var cpfCnpj = newConsulta.getCpfCnpj();
-
         return validCpfCnpjService.validCpfCnpj(cpfCnpj)
                 .flatMap(isValid -> {
                     if (isValid) {
